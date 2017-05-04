72f1d12b1b01521e21687c23115293876e5d37dd
Tony Su <peitaosu@163.com>
2017-05-04 18:20:07 +0800

    update bmp parameterizer format

--- format/bmp/parameterizer.py
+++ format/bmp/parameterizer.py
@@ -3,10 +3,9 @@ from parser.bmp import BMP
 from parser.bmp_diff import BMP_DIFF
 
 def parameterize(file_diff):
-    print "{:>48}".format("============ Parameters ============") + " : " + "{:>8}".format("before") + " <---> " + "{:<8}".format("after")
+    print "{:>48} : {:>8} <---> {:<8}".format("============ Parameters ============", "before", "after")
     for i in range(len(file_diff.attributes)):
-        print "{:>48}".format(file_diff.description[i]) + " : " + "{:>8}".format(getattr(file_diff, file_diff.attributes[i])[0]) + " <---> " + "{:<8}".format(getattr(file_diff, file_diff.attributes[i])[1])
-
+        print "{:>48} : {:>8} <---> {:<8}".format(file_diff.description[i], getattr(file_diff, file_diff.attributes[i])[0], getattr(file_diff, file_diff.attributes[i])[1])
 
 if __name__ == "__main__":
     bmp_before = BMP()
