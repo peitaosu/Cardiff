c195870c4a84ad3d8968ec2f5d3ee8b77bfe169f
Tony Su <peitaosu@163.com>
2017-05-04 19:08:22 +0800

    save diff file in txt format visualizer

--- format/txt/visualizer.py
+++ format/txt/visualizer.py
@@ -1,14 +1,25 @@
-import os, sys
+import os, sys, time
+from differ import diff
 
-def visualize(file_diff):
+def visualize(file_diff, file_output_name = None):
     default_editor = os.getenv('EDITOR')
     if default_editor:
-        os.system('%s %s' % (default_editor, file_diff))
+        if file_output_name == None:
+            file_output_name = str(time.time())
+        with open(file_output_name + ".diff.txt", "w") as diff_file:
+            for line in txt_diff:
+                diff_file.write(line)
+        os.system('%s %s' % (default_editor, file_output_name + ".diff.txt"))
     else:
-        with open(file_diff) as diff:
-            for line in diff:
-                print line
+        for line in file_diff:
+            sys.stdout.write(line)
 
 if __name__ == "__main__":
-    file_diff = sys.argv[1]
-    visualize(file_diff)
+    txt_before = sys.argv[1]
+    txt_after = sys.argv[2]
+    txt_diff = diff(txt_before, txt_after)
+    try:
+        file_output_name = sys.argv[3]
+        visualize(txt_diff, file_output_name)
+    except:
+        visualize(txt_diff)
