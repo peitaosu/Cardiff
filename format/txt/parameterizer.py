410996539890bd0767778d973c101377fd394a08
Tony Su <peitaosu@163.com>
2017-05-04 19:07:46 +0800

    update txt format parameterizer

--- format/txt/parameterizer.py
+++ format/txt/parameterizer.py
@@ -1,17 +1,19 @@
 import os, sys
+from differ import diff
 
 def parameterize(file_diff):
-    with open(file_diff) as diff:
-        add = 0
-        subtract = 0
-        for line in diff:
-            if line.startswith("+"):
-                add += 1
-            elif line.startswith("-"):
-                subtract += 1
-        print "add(+): " + str(add)
-        print "subtract(-): " + str(subtract)
+    add = 0
+    subtract = 0
+    for line in file_diff:
+        if line.startswith("+"):
+            add += 1
+        elif line.startswith("-"):
+            subtract += 1
+    print "add(+): " + str(add)
+    print "subtract(-): " + str(subtract)
 
 if __name__ == "__main__":
-    file_diff = sys.argv[1]
-    parameterize(file_diff)
+    txt_before = sys.argv[1]
+    txt_after = sys.argv[2]
+    txt_diff = diff(txt_before, txt_after)
+    parameterize(txt_diff)
