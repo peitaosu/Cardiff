81ca37c70098801b819018fb0aac7cd3c25716ca
Tony Su <peitaosu@163.com>
2017-05-05 21:16:57 +0800

    add pixel changing in parameterizer

--- format/bmp/parameterizer.py
+++ format/bmp/parameterizer.py
@@ -6,6 +6,7 @@ def parameterize(file_diff):
     print "{:>48} : {:>8} <---> {:<8}".format("============ Parameters ============", "before", "after")
     for i in range(len(file_diff.attributes)):
         print "{:>48} : {:>8} <---> {:<8}".format(file_diff.description[i], getattr(file_diff, file_diff.attributes[i])[0], getattr(file_diff, file_diff.attributes[i])[1])
+    print "{:>48} : {:>12}".format("Pixel Changed", str(len(file_diff.pixel_diff)))
 
 if __name__ == "__main__":
     bmp_before = BMP()
