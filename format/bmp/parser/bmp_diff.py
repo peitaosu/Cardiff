f265b553acbfde7a736ac41f885517e5ce5e0d26
Tony Su <peitaosu@163.com>
2017-05-05 23:53:20 +0800

    add biOffBits into parameters

--- format/bmp/parser/bmp_diff.py
+++ format/bmp/parser/bmp_diff.py
@@ -1,17 +1,28 @@
-import sys
+import sys, struct
 
+def bytes_to_int(bytes):
+    result = ord(bytes[0])
+    for b in bytes[1:]:
+        result = result + ord(b) * 256
+    return result
+
+def int_to_bytes(value, length):
+    result = []
+    for i in range(0, length):
+        result.append(value >> (i * 8) & 0xff)
+    return result
 
 class BMP_DIFF():
     def __init__(self):
-        self.attributes = ["bfSize", "biSize", "biWidth", "biHeight", "biBitCount", "biCompression",
+        self.attributes = ["bfSize", "bfOffBits", "biSize", "biWidth", "biHeight", "biBitCount", "biCompression",
                            "biSizeImage", "biXPelsPerMeter", "biYPelsPerMeter", "biClrUsed", "biClrImportant"]
-        self.description = ["The size of the file in bytes", "Header Size", "Image width in pixels", "Image height in pixels", "Bits per pixel", "Compression typ", "Image Size",
+        self.description = ["The size of the file in bytes", "Offset to start of Pixel Data", "Header Size", "Image width in pixels", "Image height in pixels", "Bits per pixel", "Compression typ", "Image Size",
                             "Preferred resolution in pixels per meter", "Preferred resolution in pixels per meter", "Number Color Map entries that are actually used", "Number of significant colors"]
 
     def diff_spec(self, field, bmp_before, bmp_after):
         setattr(self, field, [])
-        getattr(self, field).append(ord(bmp_before.get_spec_data(field)[0]))
-        getattr(self, field).append(ord(bmp_after.get_spec_data(field)[0]))
+        getattr(self, field).append(bytes_to_int(bmp_before.get_spec_data(field)))
+        getattr(self, field).append(bytes_to_int(bmp_after.get_spec_data(field)))
         getattr(self, field).append(str(getattr(self, field)[
             0]) + " <---> " + str(getattr(self, field)[1]))
 
