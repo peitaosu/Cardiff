db08de1962ef64786cd23a3b234928fdc7f4ef1a
Tony Su <peitaosu@163.com>
2017-05-11 20:28:17 +0800

    add visualize image method in visualize.py

--- visualize.py
+++ visualize.py
@@ -1,4 +1,15 @@
 import os, sys, importlib, time
+from PIL import Image, ImageTk
+import Tkinter
+
+def visualize_image(file_image):
+    window = Tkinter.Tk()
+    window.wm_title(file_image)
+    image_to_show = Image.open(file_image)
+    image_tk = ImageTk.PhotoImage(image_to_show)
+    image_label = Tkinter.Label(window, image=image_tk)
+    image_label.pack(side = "bottom", fill = "both", expand = "yes")
+    window.mainloop()
 
 def visualize_diff(file_diff, file_after, file_ext, file_output_name = None):
     file_visualizer = importlib.import_module("format." + file_ext + ".visualizer")
