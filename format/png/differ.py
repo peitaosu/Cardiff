3edd1e53648422e07c9eb1360de8639cd8cf5216
Tony Su <peitaosu@163.com>
2017-05-13 21:40:26 +0800

    save diff.json for png diff

--- format/png/differ.py
+++ format/png/differ.py
@@ -1,4 +1,4 @@
-import sys
+import sys, json
 from PIL import Image
 from parser.png_diff import PNG_DIFF
 
@@ -8,6 +8,20 @@ def diff(file_before, file_after):
     png_diff = PNG_DIFF()
     png_diff.diff(png_before, png_after)
     return png_diff
+
+def make_diff(file_before, file_after, file_output_name):
+    png_diff = diff(file_before, file_after)
+    diff_content = {}
+    for attr in png_diff.attributes:
+        diff_content[attr] = {}
+        diff_content[attr]["before"] = getattr(png_diff, attr)[0]
+        diff_content[attr]["after"] = getattr(png_diff, attr)[1]
+        if diff_content[attr]["before"] != diff_content[attr]["after"]:
+            diff_content[attr]["diff"] = True
+    diff_content["pixel"] = png_diff.pixel_diff
+    with open(file_output_name + "diff.json", "w") as diff_file:
+        json.dump(diff_content, diff_file, indent=4)
+    return file_output_name + ".diff.json"
 
 if __name__ == "__main__":
     file_before = sys.argv[1]
