58bb1af6ccdefe613782b0e02a17f64c9228717d
Tony Su <peitaosu@163.com>
2017-05-16 21:17:54 +0800

    visualizer for jpg format

---
+++ format/jpg/visualizer.py
@@ -0,0 +1,75 @@
+import os, sys, time
+from differ import diff
+from parser.jpg_diff import *
+from PIL import Image, ImageTk
+import Tkinter
+
+def visualize_as_window(file_to_show):
+    """visualize the jpg diff, open with Tkinter window
+
+    args:
+        file_to_show (str)
+    """
+    window = Tkinter.Tk()
+    window.wm_title(file_to_show)
+    image_to_show = Image.open(file_to_show)
+    image_tk = ImageTk.PhotoImage(image_to_show)
+    image_label = Tkinter.Label(window, image=image_tk)
+    image_label.pack(side = "bottom", fill = "both", expand = "yes")
+    window.mainloop()
+
+
+def visualize_as_jpg(file_diff, file_after, file_output_name = None):
+    """visualize the jpg diff, open as jpg file with alpha channel
+
+    args:
+        file_diff (JPG_DIFF)
+        file_after (str)
+        file_output_name (str)
+    
+    returns:
+        jpg_file (str)
+    """
+    for attr in file_diff.attributes:
+        if getattr(file_diff, attr)[0] != getattr(file_diff, attr)[1]:
+            return
+    image = Image.open(file_after)
+    pixel_data = image.load()
+    pixel_index = 1
+    width, height = image.size
+    for y in xrange(height):
+        for x in xrange(width):
+            if str(pixel_index) in file_diff.pixel_diff:
+                pixel_index += 1
+                continue
+            else:
+                pixel_index += 1
+                pixel_data[x, y] = (pixel_data[x, y][0], pixel_data[x, y][1], pixel_data[x, y][2])
+    if file_output_name == None:
+        file_output_name = str(time.time())
+    image.save(file_output_name + ".diff.jpg", "JPEG")
+    return file_output_name + ".diff.jpg"
+
+def visualize(file_diff, file_after, file_output_name = None):
+    """visualize the jpg diff, open with Tk window
+
+    args:
+        file_diff (JPG_DIFF)
+        file_after (str)
+        file_output_name (str)
+    """
+    if file_output_name == None:
+        file_output_name = str(time.time())
+    saved_file = visualize_as_jpg(file_diff, file_after, file_output_name)
+    visualize_as_window(saved_file)
+
+if __name__ == "__main__":
+    jpg_before = sys.argv[1]
+    jpg_after = sys.argv[2]
+    jpg_diff = JPG_DIFF()
+    jpg_diff.diff(jpg_before, jpg_after)
+    try:
+        file_output_name = sys.argv[3]
+        visualize(jpg_diff, sys.argv[2], file_output_name)
+    except:
+        visualize(jpg_diff, sys.argv[2])
