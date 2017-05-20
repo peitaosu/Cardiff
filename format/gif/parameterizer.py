414a2f1d241181be4089684936ec7b549833bf61
Tony Su <peitaosu@163.com>
2017-05-20 17:01:17 +0800

    add parameterizer for gif format

---
+++ format/gif/parameterizer.py
@@ -0,0 +1,15 @@
+import os, sys
+from PIL import Image
+from parser.gif_diff import GIF_DIFF
+
+def parameterize(file_diff):
+    """print formatted diff data
+
+    args:
+        file_diff (GIF_DIFF)
+    """
+    print "{:>48} : {:>8} <---> {:<8}".format("============ Parameters ============", "before", "after")
+    for i in range(len(file_diff.attributes)):
+        print "{:>48} : {:>8} <---> {:<8}".format(file_diff.description[i], getattr(file_diff, file_diff.attributes[i])[0], getattr(file_diff, file_diff.attributes[i])[1])
+    print "{:>48} : {:>12}".format("Pixel Changed", str(len(file_diff.pixel_diff)))
+
