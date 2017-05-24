2ab4ed4fff6664b9951e95c85af5940da9f30232
Tony Su <peitaosu@163.com>
2017-05-24 20:36:28 +0800

    add method to save wave plot as png file

--- format/wav/visualizer.py
+++ format/wav/visualizer.py
@@ -21,3 +21,25 @@ def visualize_wave_file(file_path):
     for channel in channels:
         matplotlib.pyplot.plot(Time, channel)
     matplotlib.pyplot.show()
+
+def visualize_as_png(file_path, file_output_name = None):
+    wav_file = wave.open(file_path, "r")
+
+    signal = wav_file.readframes(-1)
+    signal = numpy.fromstring(signal, "Int16")
+
+    channels = [[] for channel in range(wav_file.getnchannels())]
+    for index, datum in enumerate(signal):
+        channels[index % len(channels)].append(datum)
+
+    fs = wav_file.getframerate()
+    Time = numpy.linspace(0, len(signal) / len(channels) /
+                        fs, num=len(signal) / len(channels))
+
+    matplotlib.pyplot.figure(1)
+    matplotlib.pyplot.title(file_path)
+    for channel in channels:
+        matplotlib.pyplot.plot(Time, channel)
+
+    matplotlib.pyplot.savefig(file_output_name + ".png")
+    return file_output_name + ".png"
