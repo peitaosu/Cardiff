492ba43ef4ad830b9839d821d2fde01d1286e44e
Tony Su <peitaosu@163.com>
2017-05-28 15:36:44 +0800

    fix changed frames count issue in parameterizer and format the output

--- format/wav/parameterizer.py
+++ format/wav/parameterizer.py
@@ -6,7 +6,7 @@ def parameterize(file_diff):
     args:
         file_diff (WAV_DIFF)
     """
-    print "{:>48} : {:>8} <---> {:<8}".format("============ Parameters ============", "before", "after")
+    print "{:>48} : {:>14} <---> {:<14}".format("============ Parameters ============", "before", "after")
     for i in range(len(file_diff.attributes)):
-        print "{:>48} : {:>8} <---> {:<8}".format(file_diff.description[i], getattr(file_diff, file_diff.attributes[i])[0], getattr(file_diff, file_diff.attributes[i])[1])
-    print "{:>48} : {:>12}".format("Frame Changed", str(len(file_diff.frame_diff)))
+        print "{:>48} : {:>14} <---> {:<14}".format(file_diff.description[i], getattr(file_diff, file_diff.attributes[i])[0], getattr(file_diff, file_diff.attributes[i])[1])
+    print "{:>48} : {:>18}".format("Frame Changed", str(sum(len(v) for v in file_diff.frame_diff.itervalues())))
