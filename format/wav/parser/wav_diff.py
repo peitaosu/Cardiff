0fe16eaef531b4203de495202ccb836ae55596a7
Tony Su <peitaosu@163.com>
2017-05-29 22:03:47 +0800

    wav_diff as child class of audio_diff

--- format/wav/parser/wav_diff.py
+++ format/wav/parser/wav_diff.py
@@ -1,23 +1,10 @@
 import numpy
+from format.audio_diff import AUDIO_DIFF
 
-class WAV_DIFF():
+class WAV_DIFF(AUDIO_DIFF):
     def __init__(self):
         self.attributes = ["frames_count", "framerate", "duration", "channels_count", "sample_width", "compress_type", "compress_name"]
         self.description = ["Numbers of Frames", "Sampling Frequency", "Duration", "Numbers of Channels", "Sample Width", "Compression Type", "Compression Name"]
-
-    def diff_spec(self, field, wav_before, wav_after):
-        """diff wave specification
-
-        args:
-            field (str)
-            wav_before (WAV)
-            wav_after (WAV)
-        """
-        setattr(self, field, [])
-        getattr(self, field).append(getattr(wav_before, "get_" + field)())
-        getattr(self, field).append(getattr(wav_after, "get_" + field)())
-        getattr(self, field).append(str(getattr(self, field)[
-            0]) + " <---> " + str(getattr(self, field)[1]))
 
     def diff_frame(self, wav_before, wav_after):
         """diff wav file frames and return the frame diff
@@ -60,15 +47,3 @@ class WAV_DIFF():
             #TODO: add the diff for different count of channels
             pass
         return frame_diff
-
-    def diff(self, wav_before, wav_after):
-        """diff wav file
-
-        args:
-            wav_before (WAV)
-            wav_after (WAV)
-        """
-        for attr in self.attributes:
-            self.diff_spec(attr, wav_before, wav_after)
-        self.frame_diff = self.diff_frame(wav_before, wav_after)
-        self.frame_diff_count = sum(len(v) for v in self.frame_diff.itervalues())
