798c0140cc3c0dea5ee5c05da02f93302e07f875
Tony Su <peitaosu@163.com>
2017-06-06 22:27:51 +0800

    remove main method from diff.py and merge.py

--- diff.py
+++ diff.py
@@ -34,13 +34,3 @@ def diff(file_before, file_after):
     file_ext = file_name.split(".")[-1]
     file_differ = importlib.import_module("format." + file_ext + ".differ")
     return file_differ.diff(file_before, file_after)
-
-if __name__ == "__main__":
-    file_before = sys.argv[1]
-    file_after = sys.argv[2]
-    try:
-        file_output_name = sys.argv[3]
-        saved_file = diff_file(file_before, file_after, file_output_name)
-    except:
-        saved_file = diff_file(file_before, file_after)
-    print "File Saved: " + saved_file
--- merge.py
+++ merge.py
@@ -33,13 +33,3 @@ def merge(file_before, file_after):
     file_ext = file_name.split(".")[-1]
     file_merger = importlib.import_module("format." + file_ext + ".merger")
     return file_merger.merge(file_before, file_after)
-
-if __name__ == "__main__":
-    file_before = sys.argv[1]
-    file_after = sys.argv[2]
-    try:
-        file_output_name = sys.argv[3]
-        saved_file = merge_file(file_before, file_after, file_output_name)
-    except:
-        saved_file = merge_file(file_before, file_after)
-    print "File Saved: " + saved_file
