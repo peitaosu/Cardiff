deaea5d9068c66ebf6aec8f83835a1c0f1564e5c
Tony Su <peitaosu@163.com>
2017-06-07 21:04:44 +0800

    more friendly output in test.py

--- test.py
+++ test.py
@@ -2,27 +2,28 @@ from Cardiff import Cardiff
 
 def test_cmd_help():
     cardiff.exec_cmd(["help"])
-    cardiff.exec_cmd(["help", "log"])
 
 def test_cmd_log():
+    cardiff.exec_cmd(["help", "log"])
     cardiff.exec_cmd(["log"])
 
 def test_cmd_branch():
+    cardiff.exec_cmd(["help", "branch"])
     cardiff.exec_cmd(["branch"])
 
 if __name__ == "__main__":
     print "[TEST] New a Cardiff object..."
     cardiff = Cardiff()
-    
+
     print "[TEST] Load settings.json from Cardiff root path..."
     settings_path = "./settings.json"
     cardiff.load_settings(settings_path)
 
-    print "[TEST] Test help command..."
+    print "[TEST] Command - help"
     test_cmd_help()
 
-    print "[TEST] Test log command..."
+    print "[TEST] Command - log"
     test_cmd_log()
 
-    print "[TEST] Test branch command..."
+    print "[TEST] Command - branch"
     test_cmd_branch()
