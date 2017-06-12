8634d7e45ba91b172897e63c0d157fb86ca20cd8
Tony Su <peitaosu@163.com>
2017-06-12 22:25:10 +0800

    add class PSD for psd format

---
+++ format/psd/parser/psd.py
@@ -0,0 +1,30 @@
+from psd_tools import PSDImage
+
+class PSD():
+    def __init__(self):
+        pass
+
+    def load_psd_from_file(self, file_path):
+        self.psd_file = PSDImage.load(file_path)
+
+    def get_psd_header(self):
+        return self.psd_file.header
+
+    def get_psd_layers(self, index = None):
+        if index is not None:
+            return self.psd_file.layers[index]
+        return self.psd_file.layers
+
+    def get_decoded_data(self):
+        return self.psd_file.decoded_data
+
+    def get_embedded(self):
+        return self.psd_file.embedded
+
+    def export_single_layer(self, index, file_path):
+        layer_image = self.psd_file.layers[index].as_PIL()
+        layer_image.save(file_path)
+
+    def export_merged_image(self, file_path):
+        merged_image = self.psd_file.as_PIL()
+        merged_image.save(file_path)
