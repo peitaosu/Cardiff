f8bc3627f32b704933d377ed83657d5356ee3d36
Tony Su <peitaosu@163.com>
2017-06-13 21:10:50 +0800

    get_psd_layers method return layers information dict

--- format/psd/parser/psd.py
+++ format/psd/parser/psd.py
@@ -2,21 +2,33 @@ from psd_tools import PSDImage
 
 class PSD():
     def __init__(self):
-        pass
+        self.header_fields = ["number_of_channels", "height", "width", "depth", "color_mode"]
+        self.layer_fields = ["name", "blend_mode", "opacity", "visible"]
+        self.bbox_fields = ["height", "width", "x1", "x2", "y1", "y2"]
 
     def load_psd_from_file(self, file_path):
         self.psd_file = PSDImage.load(file_path)
 
     def get_psd_header(self):
         self.header = {}
-        for field in self.psd_file.header._fields:
+        for field in self.header_fields:
             self.header[field] = getattr(self.psd_file.header, field)
         return self.header
 
-    def get_psd_layers(self, index = None):
-        if index is not None:
-            return self.psd_file.layers[index]
-        return self.psd_file.layers
+    def get_psd_layers(self, layer_id = None):
+        self.layers = {}
+        for layer in self.psd_file.layers:
+            layer_id = str(layer.layer_id)
+            self.layers[layer_id] = {}
+            for field in self.layer_fields:
+                self.layers[layer_id][field] = getattr(layer, field)
+            bbox = {}
+            for field in self.bbox_fields:
+                bbox[field] = getattr(layer.bbox, field)
+            self.layers[layer_id]["bbox"] = bbox
+        if layer_id is not None:
+            return self.layers[layer_id]
+        return self.layers
 
     def get_decoded_data(self):
         return self.psd_file.decoded_data
