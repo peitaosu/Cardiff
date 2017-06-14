1bccd47416b608e1604ed175d86305988e7f0252
Tony Su <peitaosu@163.com>
2017-06-14 21:16:20 +0800

    code refactor for test.py

--- test.py
+++ test.py
@@ -3,38 +3,72 @@ import shutil
 import importlib
 from PIL import Image
 import json
+import pprint
+import time
+
+pp = pprint.PrettyPrinter(indent=4)
+
+def test_load_settings():
+    print "[TEST] Load settings.json from Cardiff root path..."
+    settings_path = "./settings.json"
+    with open(settings_path) as settings_file:
+        settings = json.load(settings_file)
+    settings["user.name"] = "test"
+    settings["user.email"] = "test@@cardiff"
+
+    print "Settings Detail:"
+    pp.pprint(settings)
+    with open(settings_path, "w") as settings_file:
+        json.dump(settings, settings_file, indent=4)
+    cardiff.load_settings(settings_path)
 
 def test_cmd_help():
+    print "[TEST] Command - help"
     cardiff.exec_cmd(["help"])
 
 def test_cmd_init():
+    print "[TEST] Command - init"
     cardiff.exec_cmd(["init", "./test"])
 
 def test_cmd_commit():
-    cardiff.exec_cmd(["commit", "file.bmp", "commit no.1"])
+    print "[TEST] Command - commit"
+    print "Create Dummy File: file.bmp"
+    create_dummy_bmp()
+    cardiff.exec_cmd(["commit", "file.bmp", "commit msg " + str(time.time())])
+    cardiff.exec_cmd(["log"])
 
 def test_cmd_diff():
+    print "[TEST] Command - diff"
     cardiff.exec_cmd(["diff", "file.bmp", "1", "2"])
 
 def test_cmd_merge():
+    print "[TEST] Command - merge"
     cardiff.exec_cmd(["merge", "file.bmp", "1", "2"])
 
 def test_cmd_log():
+    print "[TEST] Command - log"
     cardiff.exec_cmd(["help", "log"])
     cardiff.exec_cmd(["log"])
 
 def test_cmd_branch():
+    print "[TEST] Command - branch"
     cardiff.exec_cmd(["help", "branch"])
     cardiff.exec_cmd(["branch"])
-
-def test_switch_branch():
+    print "Commit to branch: file.bmp"
+    create_dummy_bmp()
+    cardiff.exec_cmd(["commit", "file.bmp", "commit msg " + str(time.time())])
+    cardiff.exec_cmd(["log"])
+    print "Create New Branch: new_branch"
     cardiff.exec_cmd(["branch", "new_branch"])
+    cardiff.exec_cmd(["branch"])
 
 def test_cmd_clean():
+    print "[TEST] Command - clean"
     cardiff.exec_cmd(["help", "clean"])
     cardiff.exec_cmd(["clean"])
 
 def test_rollback():
+    print "Rollback Changes"
     shutil.rmtree("./test")
 
 def create_dummy_bmp():
@@ -51,53 +85,22 @@ def create_dummy_file(ext, content):
     #TODO: create file with method of class
 
 if __name__ == "__main__":
-    print "[TEST] New a Cardiff object..."
+
+    print "[TEST] Testing for Cardiff..."
     cardiff = Cardiff()
 
-    print "[TEST] Load settings.json from Cardiff root path..."
+    test_load_settings()
 
-    settings_path = "./settings.json"
-    with open(settings_path) as settings_file:
-        settings = json.load(settings_file)
-    settings["user.name"] = "test"
-    settings["user.email"] = "test@@cardiff"
-    with open(settings_path, "w") as settings_file:
-        json.dump(settings, settings_file, indent=4)
-    cardiff.load_settings(settings_path)
-
-    print "[TEST] Command - help"
     test_cmd_help()
 
-    print "[TEST] Command - init"
     test_cmd_init()
 
-    print "[TEST] Command - branch"
     test_cmd_branch()
 
-    print "[TEST] Create Dummy File"
-    create_dummy_file("bmp", None)
-    create_dummy_bmp()
-
-    print "[TEST] Command - commit"
     test_cmd_commit()
 
-    print "[TEST] Command - diff"
-    test_cmd_commit()
-    #test_cmd_diff()
-
-    print "[TEST] Switch Branch"
-    test_switch_branch()
-
-    print "[TEST] Command - merge"
-    test_cmd_commit()
-    test_cmd_commit()
-    #test_cmd_merge()
-
-    print "[TEST] Command - log"
     test_cmd_log()
 
-    print "[TEST] Command - clean"
     test_cmd_clean()
 
-    print "[TEST] Rollback Changes"
     test_rollback()
