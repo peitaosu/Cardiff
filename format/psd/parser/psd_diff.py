8df1b038f1aa97c6601286b93d96812cf2cb5036
Tony Su <peitaosu@163.com>
2017-06-16 21:00:01 +0800

    add diff_layer method for PSD_DIFF

--- format/psd/parser/psd_diff.py
+++ format/psd/parser/psd_diff.py
@@ -1,6 +1,7 @@
 class PSD_DIFF():
     def __init__(self):
         self.header = {}
+        self.layer = {}
 
     def diff_header(self, psd_before, psd_after):
         header_before = psd_before.get_psd_header()
@@ -9,3 +10,12 @@ class PSD_DIFF():
             self.header[field] = {}
             self.header[field]["before"] = header_before[field]
             self.header[field]["after"] = header_after[field]
+
+    def diff_layer(self, psd_before, psd_after):
+        layers_before = psd_before.get_psd_layers()
+        layers_after = psd_after.get_psd_layers()
+        layer_ids = set(layers_before.keys() + layers_after.keys())
+        for layer_id in layer_ids:
+            self.layer[layer_id] = {}
+            self.layer[layer_id]["before"] = layers_before[layer_id]
+            self.layer[layer_id]["after"] = layers_after[layer_id]
