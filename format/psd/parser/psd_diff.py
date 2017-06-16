ad8d61eb1a0953de6a1d01a6807291672a8f5ca2
Tony Su <peitaosu@163.com>
2017-06-16 21:42:41 +0800

    add parameter and pixel diff method for layer

--- format/psd/parser/psd_diff.py
+++ format/psd/parser/psd_diff.py
@@ -11,11 +11,38 @@ class PSD_DIFF():
             self.header[field]["before"] = header_before[field]
             self.header[field]["after"] = header_after[field]
 
-    def diff_layer(self, psd_before, psd_after):
-        layers_before = psd_before.get_psd_layers()
-        layers_after = psd_after.get_psd_layers()
-        layer_ids = set(layers_before.keys() + layers_after.keys())
+    def diff_layer_pixel(self, layer_image_before, layer_image_after):
+        layer_image_data_before = layer_image_before.load()
+        layer_image_data_after = layer_image_after.load()
+        pixel_index = 1
+        pixel_diff = {}
+        width, height = layer_image_before.size
+        for y in xrange(height):
+            for x in xrange(width):
+                if layer_image_data_before[x, y] != layer_image_data_after[x, y]:
+                    pixel_diff[str(pixel_index)] = {}
+                    pixel_diff[str(pixel_index)]["before"] = list(layer_image_data_before[x, y])
+                    pixel_diff[str(pixel_index)]["after"] = list(layer_image_data_after[x, y])
+                pixel_index += 1
+        return pixel_diff
+
+    def diff_layer_parameters(self, layer_parameter_before, layer_parameter_after):
+        parameter_diff = {}
+        for parameter in layer_parameter_before.keys():
+            if layer_parameter_before[parameter] != layer_parameter_after[parameter]:
+                parameter_diff["before"] = layer_parameter_before[parameter]
+                parameter_diff["after"] = layer_parameter_after[parameter]
+        return parameter_diff
+
+    def diff_layers(self, psd_before, psd_after):
+        layers_parameter_before = psd_before.get_psd_layers()
+        layers_parameter_after = psd_after.get_psd_layers()
+        layer_ids = set(layers_parameter_before.keys() + layers_parameter_after.keys())
         for layer_id in layer_ids:
             self.layer[layer_id] = {}
-            self.layer[layer_id]["before"] = layers_before[layer_id]
-            self.layer[layer_id]["after"] = layers_after[layer_id]
+            self.layer[layer_id]["parameter"] = self.diff_layer_parameters(layers_parameter_before[layer_id], layers_parameter_after[layer_id])
+            layer_image_before = psd_before.get_single_layer_image(int(layer_id))
+            layer_image_after = psd_after.get_single_layer_image(int(layer_id))
+            self.layer[layer_id]["pixel"] = self.diff_layer_pixel(layer_image_before, layer_image_after)
+
+            
