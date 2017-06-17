7dde8b5959ac7b14a65850549bc4bbfd86192e72
Tony Su <peitaosu@163.com>
2017-06-18 00:38:08 +0800

    add docstring for class PSD

--- format/psd/parser/psd.py
+++ format/psd/parser/psd.py
@@ -7,15 +7,33 @@ class PSD():
         self.bbox_fields = ["height", "width", "x1", "x2", "y1", "y2"]
 
     def load_psd_from_file(self, file_path):
+        """load psd from file
+
+        args:
+            file_path (str)
+        """
         self.psd_file = PSDImage.load(file_path)
 
     def get_psd_header(self):
+        """get psd header
+
+        returns:
+            header (dict)
+        """
         self.header = {}
         for field in self.header_fields:
             self.header[field] = getattr(self.psd_file.header, field)
         return self.header
 
     def get_psd_layers(self, layer_id = None):
+        """get psd layers
+
+        args:
+            layer_id (int)
+
+        returns:
+            layers (dict)
+        """
         self.layers = {}
         for layer in self.psd_file.layers:
             l_id = str(layer.layer_id)
@@ -31,21 +49,55 @@ class PSD():
         return self.layers
 
     def get_decoded_data(self):
+        """get psd decoded data
+
+        returns:
+            decoded_data (psd_tools.reader.reader.ParseResult)
+        """
         return self.psd_file.decoded_data
 
     def get_embedded(self):
+        """get psd embedded
+
+        returns:
+            embedded (list)
+        """
         return self.psd_file.embedded
 
     def get_single_layer_image(self, layer_id):
+        """get psd single layer image
+
+        args:
+            layer_id (int)
+        
+        returns:
+            layer (PIL.Image)
+        """
         for layer in self.psd_file.layers:
             if layer.layer_id == layer_id:
                 return layer.as_PIL()
 
     def get_merged_image(self):
+        """get psd merged image
+
+        returns:
+            psd (PIL.Image)
+        """
         return self.psd_file.as_PIL()
 
     def export_single_layer(self, layer_id, file_path):
+        """export single layer image
+
+        args:
+            layer_id (int)
+            file_path (str)
+        """
         self.get_single_layer_image(layer_id).save(file_path)
 
     def export_merged_image(self, file_path):
+        """export merged image
+
+        args:
+            file_path (str)
+        """
         self.get_merged_image().save(file_path)
