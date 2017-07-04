0d673ad86005ef00bec3e8ceec4eda43bdd62fa3
Tony Su <peitaosu@163.com>
2017-07-04 23:52:30 +0800

    add placeholder for psd merger

---
+++ format/psd/merger.py
@@ -0,0 +1,11 @@
+import os, sys
+from PIL import Image
+from parser.psd import PSD
+from parser.psd_diff import PSD_DIFF
+
+
+def merge(file_before, file_after):
+    pass
+
+def make_merged(file_before, file_after, file_output_name):
+    pass
