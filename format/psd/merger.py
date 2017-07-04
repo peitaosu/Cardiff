4333ee65ec03b9f788aeb53c8a887353ab3fd459
Tony Su <peitaosu@163.com>
2017-07-05 00:31:19 +0800

    add pixel merge for psd

--- format/psd/merger.py
+++ format/psd/merger.py
@@ -16,13 +16,46 @@ def merge(file_before, file_after):
             print "Header - {} not the same. Before: {} After: {}".format(param, psd_diff.header[param]["before"], psd_diff.header[param]["after"])
             print "Cannot be merged."
             return -1
+    if "AUTO_MERGE" not in os.environ:
+        option = raw_input("Choose your merge option: 1-All, 2-Pixel By Pixel: ")
+    else:
+        option = os.getenv("AUTO_MERGE")
+    layer_merged = {}
     for layer_id in psd_diff.layer:
         for param in psd_diff.layer[layer_id]["parameter"]:
             if psd_diff.layer[layer_id]["parameter"][param]["before"] != psd_diff.layer[layer_id]["parameter"][param]["after"]:
                 print "Layer {} Parameter - {} not the same. Before: {} After: {}".format(layer_id, param, psd_diff.layer[layer_id]["parameter"][param]["before"], psd_diff.layer[layer_id]["parameter"][param]["after"])
                 print "Cannot be merged."
                 return -1
-    pass
+        if psd_diff.layer[layer_id]["pixel"] != "Empty Layer." and len(psd_diff.layer[layer_id]["pixel"]) > 0:
+            layer_merged[layer_id] = {}
+            print "Layer " + layer_id + " changed. Need merge."
+            if option == "1":
+                if "AUTO_ACCEPT" not in os.environ:
+                    accept = raw_input(
+                        "Choose your merge option: 1-Accept After, 2-Accept Before: ")
+                else:
+                    accept = os.getenv("AUTO_ACCEPT")
+                for pixel_index in psd_diff.layer[layer_id]["pixel"]:
+                    if accept == "1":
+                        layer_merged[layer_id][pixel_index] = psd_diff.layer[layer_id]["pixel"][pixel_index]["after"]
+                    elif accept == "2":
+                        layer_merged[layer_id][pixel_index] = psd_diff.layer[layer_id]["pixel"][pixel_index]["before"]
+            elif option == "2":
+                for pixel_index in psd_diff.layer[layer_id]["pixel"]:
+                    print "Pixel: " + pixel_index
+                    print "Before: " + psd_diff.layer[layer_id]["pixel"][pixel_index]["before"]
+                    print "After: " + psd_diff.layer[layer_id]["pixel"][pixel_index]["after"]
+                    if "AUTO_ACCEPT" not in os.environ:
+                        accept = raw_input(
+                            "Choose your merge option: 1-Accept After, 2-Accept Before: ")
+                    else:
+                        accept = os.getenv("AUTO_ACCEPT")
+                    if accept == "1":
+                        layer_merged[layer_id][pixel_index] = psd_diff.layer[layer_id]["pixel"][pixel_index]["after"]
+                    elif accept == "2":
+                        layer_merged[layer_id][pixel_index] = psd_diff.layer[layer_id]["pixel"][pixel_index]["before"]
+    return layer_merged
 
 def make_merged(file_before, file_after, file_output_name):
     pass
